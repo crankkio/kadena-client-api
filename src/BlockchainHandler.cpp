8f14e79588e1fe0f60c81716456271bf3084fb2c
Jorge Blanco Alonso <jorge.blancoalonso@epfl.ch>
2025-05-11 20:13:25 +0200

    Try local endpoint

--- src/BlockchainHandler.cpp
+++ src/BlockchainHandler.cpp
@@ -94,7 +94,7 @@ JsonDocument BlockchainHandler::createCommandObject(const String &command, const
     JsonObject meta = cmdObject["meta"].to<JsonObject>();
     meta["creationTime"] = getCurrentUnixTime();
     meta["ttl"] = 28800;
-    meta["chainId"] = "0";
+    meta["chainId"] = "19";
     meta["gasPrice"] = 1e-7;
     meta["gasLimit"] = 2500;
     meta["sender"] = "k:" + public_key_;
@@ -261,7 +261,7 @@ BlockchainStatus BlockchainHandler::executeTransfer(const String& receiver, cons
     params.amount = amount;
     params.tokenContract = tokenContract;
 
-    return executeBlockchainCommand("send", command, params);
+    return executeBlockchainCommand("local", command, params);
 }
 
 // Function to convert enum to string
